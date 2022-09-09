4738bf46b01db471929b4e7944dc71a8b1cc69ae
xojiakbarsiddikov <siddikov.wr2007@gmail.com>
2022-09-09 19:02:16 +0500

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="message">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message-v",
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.message {
+  text-align: center;
+  padding: 20px;
+}
+p {
+  color: #ba3838;
+}
+</style>
---
+++ src/components/NewNote.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="new-note">
+    {{/* eslint-disable */}}
+    <label>Title</label>
+    <input v-model="note.title" type="text">
+    <label>Description</label>
+    <textarea v-model="note.description"></textarea>
+    <button class="btn btnPrimary" @click="addNote">New note</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+
+  name: "NewNote-v",
+  props: {
+    note: {
+      type: Object
+    }
+  },
+  methods: {
+    addNote () {
+      console.log(this.note)
+      this.$emit('addNote', this.note)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.new-note {
+  text-align: center;
+}
+</style>
+
